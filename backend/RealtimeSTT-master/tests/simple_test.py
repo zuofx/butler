03d3a30a15486704a8bf9b649af4bd1a19d343c3
Gary Chen <122473830+Gary-XC@users.noreply.github.com>
2024-02-03 18:15:03 -0500

    reading in prompts from json

--- backend/RealtimeSTT-master/tests/simple_test.py
+++ backend/RealtimeSTT-master/tests/simple_test.py
@@ -1,6 +1,7 @@
 from RealtimeSTT import AudioToTextRecorder
 from anyio import open_file
 # library that was used for real-time TTS 
+import json
 
 wordList = []
 # Storing the sentences that were said to figure out what is being mentioned in real-time
@@ -8,37 +9,52 @@ wordList = []
 # prompts
 prompt1 = "Test 1."
 
+f = open("C:\\Users\\Xi Chen\\Documents\\GitHub\\QHACKS24\\controlpanel\\public\\data\\techniques.json")
 
-runTime = True
-# variable to keep track of when the while loop is to be stopped
+data = json.load(f)
 
-if __name__ == '__main__':
-    recorder = AudioToTextRecorder(spinner=False, model="tiny.en", language="en")
+for i in data:
+    print(i)
 
-    print("Say something...")
-    # prompt for the user
-    while (runTime): 
-        wordList.append(recorder.text())
-        # adding new sentences to the wordList to keep track of 
 
-        print(recorder.text(), end=" ", flush=True)
-        # printing out the words as the sentence is being said in close to real-time, keep until testing is finished
+# runTime = True
+# # variable to keep track of when the while loop is to be stopped
 
-        if(wordList[-1].__contains__("Stop.")):
-            # Test the if statement, to see if the 
-            # If you add the code to a list, and check the last element in the list after you appended it, then the if statements can be used to flag certain phrases
-            # Although since the function recorder.text() is giving you a string of the sentence, you can use the string functions to check for certain phrases
-            # So instead of just the last element in the wordList, we can just use the recorder.text() for some better efficiency
-            """
-            Future Testing:
-            
-            """
-            print("Understood. Stopping the program.")
-            runTime = False
+# if __name__ == '__main__':
+#     recorder = AudioToTextRecorder(spinner=False, model="tiny.en", language="en")
 
-        if(prompt1 in wordList[-1]):
-            print("Prompt 1 was said")
+#     print("Say something...")
+#     # prompt for the user
+#     while (runTime): 
+#         wordList.append(recorder.text())
+#         # adding new sentences to the wordList to keep track of 
 
-            exec(open("C:\\Users\\Xi Chen\\Documents\\GitHub\\QHACKS24\\backend\\scripts\\script1.py").read(), globals())
+#         if(prompt1 in wordList[-1]):
+#             print("Prompt 1 was said")
 
-    print("You said: ", wordList)   
+#             exec(open("C:\\Users\\Xi Chen\\Documents\\GitHub\\QHACKS24\\backend\\scripts\\script1.py").read(), globals())
+
+#     print("You said: ", wordList)   
+
+
+
+
+
+
+
+
+"""
+print(recorder.text(), end=" ", flush=True)
+# printing out the words as the sentence is being said in close to real-time, keep until testing is finished
+
+if(wordList[-1].__contains__("Stop.")):
+    # Test the if statement, to see if the 
+    # If you add the code to a list, and check the last element in the list after you appended it, then the if statements can be used to flag certain phrases
+    # Although since the function recorder.text() is giving you a string of the sentence, you can use the string functions to check for certain phrases
+    # So instead of just the last element in the wordList, we can just use the recorder.text() for some better efficiency
+    
+    Future Testing:
+    
+    print("Understood. Stopping the program.")
+    runTime = False
+"""
