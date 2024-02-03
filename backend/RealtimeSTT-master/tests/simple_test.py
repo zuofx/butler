509babbdb28cd93ae04790750a26d39a1e51243b
Gary Chen <122473830+Gary-XC@users.noreply.github.com>
2024-02-02 21:56:06 -0500

    Pushing Backend Files

---
+++ backend/RealtimeSTT-master/tests/simple_test.py
@@ -0,0 +1,20 @@
+from RealtimeSTT import AudioToTextRecorder
+import sys
+
+wordList = []
+
+runTime = True
+
+if __name__ == '__main__':
+    recorder = AudioToTextRecorder(spinner=False, model="tiny.en", language="en")
+
+    print("Say something...")
+    while (runTime): 
+        print(recorder.text(), end=" ", flush=True)
+        wordList.append(recorder.text())
+        if(wordList[-1].__contains__("Stop.")):
+            print("Understood. Stopping the program.")
+            runTime = False
+
+    print("You said: ", wordList)
+
