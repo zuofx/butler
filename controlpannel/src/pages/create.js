e5c739bc61e23ab9e1b54357f4b6633c3f505dfe
Owen Sawler <owensawler4@gmail.com>
2024-02-02 23:48:30 -0500

    made colour picker

--- controlpannel/src/pages/create.js
+++ controlpannel/src/pages/create.js
@@ -1,7 +1,11 @@
+import ColourPicker from '../components/colourpicker.js'
+
+
 const Create = () => {
     return(
         <div>
             <h1>CREATE</h1>
+            <ColourPicker/>
         </div>
     )
 }
