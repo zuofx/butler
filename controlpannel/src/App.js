c1cde40940cb47bd46c18e217ab80a19910cfedd
Owen Sawler <owensawler4@gmail.com>
2024-02-02 20:39:41 -0500

    added two routes: home/create

--- controlpannel/src/App.js
+++ controlpannel/src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
 
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+//Route Imports
+import Home from './pages/home'
+import Create from './pages/create'
+
+
+
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ controlpannel/src/pages/create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return(
+        <div>
+            <h1>CREATE</h1>
+        </div>
+    )
+}
+
+export default Create
