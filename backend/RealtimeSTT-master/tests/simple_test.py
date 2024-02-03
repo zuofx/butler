6fbb44e1be7995b3e4da9088c2df9e95470075e0
Gary Chen <122473830+Gary-XC@users.noreply.github.com>
2024-02-03 01:11:02 -0500

    Documentation

--- backend/RealtimeSTT-master/tests/simple_test.py
+++ backend/RealtimeSTT-master/tests/simple_test.py
@@ -1,18 +1,31 @@
 from RealtimeSTT import AudioToTextRecorder
+# library that was used for real-time TTS 
+
 wordList = []
+# Storing the sentences that were said to figure out what is being mentioned in real-time
 
 runTime = True
+# variable to keep track of when the while loop is to be stopped
 
 if __name__ == '__main__':
     recorder = AudioToTextRecorder(spinner=False, model="tiny.en", language="en")
 
     print("Say something...")
+    # prompt for the user
     while (runTime): 
         print(recorder.text(), end=" ", flush=True)
+        # printing out the words as the sentence is being said in close to real-time, keep until testing is finished
         wordList.append(recorder.text())
+        # adding new sentences to the wordList to keep track of 
         if(wordList[-1].__contains__("Stop.")):
+            # Test the if statement, to see if the 
+            # If you add the code to a list, and check the last element in the list after you appended it, then the if statements can be used to flag certain phrases
+            # Although since the function rec
+            """
+            Future Testing:
+            
+            """
             print("Understood. Stopping the program.")
             runTime = False
 
-    print("You said: ", wordList)
-
+    print("You said: ", wordList)   
